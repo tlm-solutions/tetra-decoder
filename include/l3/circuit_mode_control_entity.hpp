3df361681a057af64736d482bb7d2847568a2c87
Markus Schmidl <markus.schmidl@mailbox.tu-dresden.de>
2023-05-02 16:46:53 +0200

    fix mac fragmentation. start implementing SDS simple text messaging

--- include/l3/circuit_mode_control_entity.hpp
+++ include/l3/circuit_mode_control_entity.hpp
@@ -9,17 +9,23 @@
 #ifndef L3_CMCE_HPP
 #define L3_CMCE_HPP
 
+#include <memory>
+
+#include <l3/short_data_service.hpp>
 #include <utils/bit_vector.hpp>
 
 class CircuitModeControlEntity {
   public:
-    CircuitModeControlEntity() noexcept = default;
+    CircuitModeControlEntity()
+        : sds_(std::make_shared<ShortDataService>()){};
     ~CircuitModeControlEntity() noexcept = default;
 
     void process(bool is_downlink, BitVector& vec);
 
   private:
     void process_d_sds_data(BitVector& vec);
+
+    std::shared_ptr<ShortDataService> sds_;
 };
 
 #endif
