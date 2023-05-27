09914df5d5be6a3f9ae79e6e5c0d90abdd8b4960
Markus Schmidl <markus.schmidl@mailbox.tu-dresden.de>
2023-05-27 16:35:58 +0200

    add initial python script for data sending

--- nixos-modules/default.nix
+++ nixos-modules/default.nix
@@ -21,11 +21,18 @@ in {
     };
     instances = mkOption {
       type = types.attrsOf (types.submodule {
-        options.decoderPort = mkOption {
+        options.receivePort = mkOption {
           type = types.port;
           default = 42000;
           description = ''
             Port where the decoder receives its bits on.
+          '';
+        };
+        options.transmitPort = mkOption {
+          type = types.port;
+          default = 42100;
+          description = ''
+            Port where the decoder transmits its data on.
           '';
         };
         options.extraDecoderArgs = mkOption {
@@ -45,14 +52,48 @@ in {
 
   config = lib.mkIf cfg.enable {
     systemd.services = lib.concatMapAttrs (instanceName: instanceConfig: {
-      "tetra-decoder-decoder-${instanceName}" = {
+      "tetra-decoder-${instanceName}" = {
         enable = true;
         wantedBy = [ "multi-user.target" ];
 
         script = ''
           exec ${pkgs.tetra-decoder}/bin/tetra-decoder --rx ${
-            toString instanceConfig.decoderPort
-          } ${instanceConfig.extraDecoderArgs} &
+            toString instanceConfig.receivePort
+          } --tx ${toString instanceConfig.transmitPort} ${instanceConfig.extraDecoderArgs} &
+        '';
+
+        serviceConfig = {
+          Type = "forking";
+          User = cfg.user;
+          Restart = "on-failure";
+          StartLimitBurst = "2";
+          StartLimitIntervalSec = "150s";
+        };
+      };
+
+      "tetra-sender-${instanceName}" = {
+        enable = true;
+        wantedBy = [ "multi-user.target" ];
+
+        script = let
+          pythonScript = lib.writeScript "tetra-sender-${instanceName}" ''
+            #!/usr/bin/env python3
+            # -*- coding: utf-8 -*-
+
+            import json
+
+            UDP_IP = "127.0.0.1"
+            UDP_PORT = ${toString instanceConfig.transmitPort}
+
+            sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            sock.bind((UDP_IP, UDP_PORT))
+
+            while True:
+              data, addr = sock.recvfrom(4096)
+              print(f"received message: {data}")
+          '';
+        in ''
+          exec pythonScript
         '';
 
         serviceConfig = {
