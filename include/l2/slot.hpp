d59f4efaf8faee26585fff81d26a774ab66be260
Markus Schmidl <markus.schmidl@mailbox.tu-dresden.de>
2024-06-14 13:21:23 +0200

    Slots: move code to cpp file

--- include/l2/slot.hpp
+++ include/l2/slot.hpp
@@ -111,99 +111,10 @@ class Slots {
     Slots(const Slots&) = default;
 
     /// constructor for one subslot or a full slot
-    Slots(BurstType burst_type, SlotsType slot_type, Slot&& slot)
-        : burst_type_(burst_type)
-        , slot_type_(slot_type)
-        , slots_({std::move(slot)}) {
-        if (slot_type_ == SlotsType::kTwoSubslots) {
-            throw std::runtime_error("Two subslots need to to have two subslots");
-        }
-
-        /// If we processes the normal uplink burst assume that the slot is signalling and not traffic. We would need
-        /// the information from the access assignment from the corresponding downlink timeslot to make the right
-        /// decision here.
-        if (burst_type_ == BurstType::NormalUplinkBurst) {
-            get_first_slot().select_logical_channel(LogicalChannel::kSignalingChannelFull);
-        }
-
-        if (!get_first_slot().is_concreate()) {
-            throw std::runtime_error("The first or only slot is not concreate.");
-        }
-    };
+    Slots(BurstType burst_type, SlotsType slot_type, Slot&& slot);
 
     /// construct for two half slot
-    Slots(BurstType burst_type, SlotsType slot_type, Slot&& first_slot, Slot&& second_slot)
-        : burst_type_(burst_type)
-        , slot_type_(slot_type)
-        , slots_({std::move(first_slot), std::move(second_slot)}) {
-        if (slot_type_ != SlotsType::kTwoSubslots) {
-            throw std::runtime_error("Only two subslots is allowed to have two subslots");
-        }
-
-        if (!get_first_slot().is_concreate()) {
-            throw std::runtime_error("The first subslot is not concreate.");
-        }
-
-        const auto& first_slot_data = get_first_slot().get_logical_channel_data_and_crc().data;
-        if (get_first_slot().get_logical_channel_data_and_crc().channel == LogicalChannel::kStealingChannel) {
-            // The first subslot is stolen, now we need to decide if the second is also stolen or traffic
-            auto second_half_slot_stolen = false;
-
-            if (burst_type_ == BurstType::NormalUplinkBurstSplit) {
-                auto pdu_type = first_slot_data.look<2>(0);
-                // read the stolen flag from the MAC-DATA
-                // 21.4.2.3 MAC-DATA
-                if (pdu_type == 0b00) {
-                    auto address_type = first_slot_data.look<2>(4);
-                    auto length_indication_or_capacity_request_offset = 6 + (address_type == 0b01 ? 10 : 24);
-                    auto length_indication_or_capacity_request =
-                        first_slot_data.look<1>(length_indication_or_capacity_request_offset);
-                    if (length_indication_or_capacity_request == 0b0) {
-                        auto length_indication =
-                            first_slot_data.look<6>(length_indication_or_capacity_request_offset + 1);
-                        if (length_indication == 0b111110 || length_indication == 0b111111) {
-                            second_half_slot_stolen = true;
-                        }
-                    }
-                }
-
-                // read the stolen flag from the MAC-U-SIGNAL PDU
-                // 21.4.5 TMD-SAP: MAC PDU structure for U-plane signalling
-                if (pdu_type == 0b11) {
-                    if (first_slot_data.look<1>(2)) {
-                        second_half_slot_stolen = true;
-                    };
-                }
-            }
-
-            if (burst_type_ == BurstType::NormalDownlinkBurstSplit) {
-                auto pdu_type = first_slot_data.look<2>(0);
-                // read the sloten flag from the MAC-RESOURCE PDU
-                // 21.4.3.1 MAC-RESOURCE
-                if (pdu_type == 0b00) {
-                    auto length_indication = first_slot_data.look<6>(7);
-                    if (length_indication == 0b111110 || length_indication == 0b111111) {
-                        second_half_slot_stolen = true;
-                    }
-                }
-
-                // read the stolen flag from the MAC-U-SIGNAL PDU
-                // 21.4.5 TMD-SAP: MAC PDU structure for U-plane signalling
-                if (pdu_type == 0b11) {
-                    if (first_slot_data.look<1>(2)) {
-                        second_half_slot_stolen = true;
-                    };
-                }
-            }
-
-            get_second_slot().select_logical_channel(second_half_slot_stolen ? LogicalChannel::kStealingChannel
-                                                                             : LogicalChannel::kTrafficChannel);
-        }
-
-        if (!get_second_slot().is_concreate()) {
-            throw std::runtime_error("The second slot is not concreate.");
-        }
-    };
+    Slots(BurstType burst_type, SlotsType slot_type, Slot&& first_slot, Slot&& second_slot);
 
     /// access the first slot
     [[nodiscard]] auto get_first_slot() noexcept -> Slot& { return slots_.front(); };
@@ -220,23 +131,7 @@ class Slots {
     [[nodiscard]] auto has_second_slot() const noexcept -> bool { return slots_.size() == 2; }
 
     // check if there was any crc mismatch on the signalling or stealing channels
-    [[nodiscard]] auto has_crc_error() -> bool {
-        auto error = false;
-
-        const auto& first_slot = slots_.front().get_logical_channel_data_and_crc();
-        if (first_slot.channel != LogicalChannel::kTrafficChannel) {
-            error |= !first_slot.crc_ok;
-        }
-
-        if (has_second_slot()) {
-            const auto& second_slot = slots_.at(1).get_logical_channel_data_and_crc();
-            if (second_slot.channel != LogicalChannel::kTrafficChannel) {
-                error |= !first_slot.crc_ok;
-            }
-        }
-
-        return error;
-    };
+    [[nodiscard]] auto has_crc_error() -> bool;
 
     /// get the type of the underlying burst
     [[nodiscard]] auto get_burst_type() const noexcept -> BurstType { return burst_type_; }
