43f3e2283c25e02dd1642b073815bfc6f3ba1540
Markus Schmidl <markus.schmidl@mailbox.tu-dresden.de>
2025-01-06 12:11:38 +0100

    update nixpkgs flake input. fix build.

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = github:NixOS/nixpkgs/nixos-23.11;
+    nixpkgs.url = github:NixOS/nixpkgs/nixos-24.11;
 
     utils.url = "github:numtide/flake-utils";
   };
@@ -9,7 +9,17 @@
     utils.lib.eachDefaultSystem
       (system:
         let
-          pkgs = nixpkgs.legacyPackages.${system};
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [
+              (self: super: {
+                armadillo = super.armadillo.overrideAttrs(oldAttrs: rec {
+                  # when using lapack derived from openblas instead of the reference implementation the build tetra-decoder executable results in having errors when creating pthreads
+                  buildInputs = with super; [ blas lapack-reference superlu ];
+                });
+              })
+            ];
+          };
           tetra-decoder = pkgs.callPackage ./derivation.nix { };
         in
         rec {
